3a68ef1aa5bd8667200fe6596d63e92fea630bfd
Odysseas.eth <odyslam@gmail.com>
2022-07-27 12:29:28 +0200

    feat: add multi-anvil

--- multi-anvil.sh
+++ multi-anvil.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-source $(dirname "$0")/terminal-helpers.sh
+source $(dirname "$0")/print-utils.sh
+set -e
 
 count=$1
 counter=0
 announce "Welcome to Multi Anvil"
-message "Mullti Anvil will generate ${count} anvil instances"
+info "Mullti Anvil will generate ${count} anvil instances"
 anvils=""
 for i in $(seq $(($count - 1))); do
   port=$(( 8545 + counter ))
   anvils="${anvils} anvil -p ${port} &"
-  message "Anvil instance live at port: ${port}"
+  info "Anvil instance live at port: ${port}"
   counter=$((counter + 1))
 done
 port=$(( 8545 + counter ))
 anvils="${anvils} anvil -p ${port}"
-message "Anvil instance live at port: ${port}"
+info "Anvil instance live at port: ${port}"
 (trap 'kill 0' SIGINT; eval "${anvils}")
