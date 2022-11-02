a256d825fd2476ccbd71315e638c1a9d852dbefa
Odysseas.eth <odyslam@gmail.com>
2022-11-02 17:09:34 -0500

    feat: add output multiplexing

--- multi-anvil.sh
+++ multi-anvil.sh
@@ -11,11 +11,11 @@ anvils=""
 # shellcheck disable=SC2034
 for i in $(seq $((count - 1))); do
   port=$(( 8545 + counter ))
-  anvils="${anvils} anvil -p ${port} &"
+  anvils="${anvils} anvil -p ${port} -b 1 | sed -e 's/^/  anvil[${port}]: /' &"
   info "Anvil instance live at port: ${port}"
   counter=$((counter + 1))
 done
 port=$(( 8545 + counter ))
-anvils="${anvils} anvil -p ${port}"
+anvils="${anvils} anvil -p ${port} -b 1 | sed -e 's/^/  anvil[${port}]: /'"
 info "Anvil instance live at port: ${port}"
 (trap 'kill 0' SIGINT; eval "${anvils}")
